ecf96218e078bc3deede30abde0ddb7be1bc13a6
Nathan de Paz <nathan.depaz@wework.com>
2018-02-13 09:28:39 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,15 +2,14 @@
 
 def fizzbuzz(int)
   
-  if 
-        "Fizz"
+  if int % 3 == 0 && int % 5 == 0
+        "FizzBuzz"
   elsif
      int % 5 == 0 
         "Buzz"
   elsif
      int % 3 == 0
         "Fizz" 
-        
   end
   
 end
