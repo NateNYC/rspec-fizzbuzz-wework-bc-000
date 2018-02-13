68745ad7b99d821de42db3eb838f91297d4c85e5
Nathan de Paz <nathan.depaz@wework.com>
2018-02-13 09:15:23 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+#FizzBuzz Code by Nathan de Paz
+
+def fizzbuzz(int)
+  
+  if int % 3 == 0
+        "Fizz"
+  elsif
+     int % 5 == 0 
+        "Buzz"
+  elsif
+     int % 3 == 
+  
+  end
+  
+
+end
